d60968d29c6035500804a880101fa4287398fac9
kushwahavipin <kushwahavipin786@gmail.com>
2025-02-02 20:14:40 +0530

    update

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
