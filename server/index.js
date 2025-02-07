ecaaf08c3aa968152a1492ae972d9369b8c119b6
kushwahavipin <kushwahavipin786@gmail.com>
2025-02-07 22:19:42 +0530

    update frontend

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://taskmanager-ochre-two.vercel.app"],
+    origin: ["https://tm-handler-frontend.vercel.app"],
     credentials:true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://taskmanager-ochre-two.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://tm-handler-frontend.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
